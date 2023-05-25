6bc1862d9c2c5d2849126e1e5461b6c9622b8020
Adem Koran <ademkoran2525@gmail.com>
2023-05-25 19:33:40 +0300

    degisiklik yapildi2

--- src/main/java/Adem01/C01_Test.java
+++ src/main/java/Adem01/C01_Test.java
@@ -3,6 +3,7 @@ package Adem01;
 public class C01_Test {
     public static void main(String[] args) {
         System.out.println("hello word");
+        System.out.println("hello word");
     }
 
 }
